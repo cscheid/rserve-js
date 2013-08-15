7f13bbf4735fee0221039e59e05aaf97fed81951
Carlos Scheidegger <cscheid@research.att.com>
2013-08-15 19:11:38 -0400

    cleanup

--- tests/basic.js
+++ tests/basic.js
@@ -1,17 +1,5 @@
 Rserve = require('../main.js');
 
-function sequence_(lst)
-{
-    function do_it(i) {
-        if (i === lst.length)
-            return;
-        lst[i](function() { 
-            do_it(i+1);
-        });
-    }
-    do_it(0);
-}
-
 s = Rserve.create({
     host: 'http://127.0.0.1:8081',
     on_connect: test
